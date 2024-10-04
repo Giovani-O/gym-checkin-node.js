10abc5aac63ca19ce44963af284f24c84e7703b9
Giovani <giovani98@proton.me>
2024-10-04 14:17:48 -0300

    feat: Initial project settings

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+// 1. Create validation schema with zod
+// 2. Validate the environment variables with safeParse
+// 3. Throw an error if they are invalid
+// 4. Exports the environment variables if they are valid
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables!', _env.error.format())
+
+  throw new Error('Invalid environment variables!')
+}
+
+export const env = _env.data
