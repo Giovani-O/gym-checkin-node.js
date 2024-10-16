b6442c82ef50e18cb0ab7e5225bd56954c316895
Giovani <giovani98@proton.me>
2024-10-16 14:00:18 -0300

    feat: Add profile controller

--- src/use-cases/_factories/make-get-user-profile-use-case.ts
+++ src/use-cases/_factories/make-get-user-profile-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '../authenticate/authenticate'
+import { GetUserProfileUseCase } from '../get-user-profile/get-user-profile'
 
 export function makeGetUserProfileUseCase() {
   const usersRepository = new PrismaUsersRepository()
-  const useCase = new AuthenticateUseCase(usersRepository)
+  const useCase = new GetUserProfileUseCase(usersRepository)
 
   return useCase
 }
