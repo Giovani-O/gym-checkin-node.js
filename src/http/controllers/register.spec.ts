7873980e89d47fd32ac15dcdd611371f2411bbb4
Giovani <giovani98@proton.me>
2024-10-17 09:08:48 -0300

    feat: Create e2e test for register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@mail.com',
+      password: 'Abc123!@',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
