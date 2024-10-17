01610adf7b607018c15deb2f9cec9a4b0716fc1d
Giovani <giovani98@proton.me>
2024-10-17 09:12:13 -0300

    feat: Create e2e test for authentication

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@mail.com',
+      password: 'Abc123!@',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'john.doe@mail.com',
+      password: 'Abc123!@',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
